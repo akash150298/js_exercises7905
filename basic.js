82ff04a31d1b4dcace14452c9dc8fe0f3ae11ce9
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-02 07:16:10 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,13 @@
+let name = "Akash-samant fjrrnk"
+let age = 20
+const gameName = new String('hitesh-hc-com')
+console.log(name.charAt(2))
+console.log(name.indexOf('a'))
+console.log(name.replace(' ',"waah"))
+console.log(name.includes('Akas'))
+console.log(name.split('-'))
+console.log(Math.floor(Math.random()*10) + 1)
+max = 20
+min = 10
+console.log(Math.floor(Math.random()*(max- min + 1)) + min)
+
