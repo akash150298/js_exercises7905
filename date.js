8f656d5c91717ad07ab63cf0c7fd73b34d1ed948
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-02 09:47:51 +0530

    Create date.js

---
+++ date.js
@@ -0,0 +1,14 @@
+const date = new Date()
+console.log(date)
+console.log(date.toString())
+console.log(date.toLocaleString())
+console.log(new Date())
+let present = Date.now()
+const myCreatedDate = new Date('01-14-2023')
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDate());
