62a20ac891e8f4297ff770da9b6abea56ced1ff2
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-05 15:54:20 +0530

    Update iter.js

--- basic/iter.js
+++ basic/iter.js
@@ -13,3 +13,18 @@ countryCodesMap.set('IN', "India");
 for (const [code, country] of countryCodesMap) {
     console.log(`Key: ${code} Value: ${country}`);
 }
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+ for(const key in myObject) {
+     console.log(`${key} ${myObject[key]}`)
+ }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
