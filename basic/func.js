d9650929e77aca0699cd57b897669de0ba1f8539
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-05 15:11:07 +0530

    playingwithFunctions

--- basic/func.js
+++ basic/func.js
@@ -2,3 +2,10 @@ function Calculate(num1, num2, ...num) {
     return num
 }
 console.log(Calculate(4,5,6,9))
+
+function chai() {
+    let username = "hitesh"
+    console.log(this);
+}
+const c = chai
+console.log(c)
