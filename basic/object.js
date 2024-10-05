e1db918c373426707c891528298a0b678edc63ea
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-05 14:00:15 +0530

    Update object.js

--- basic/object.js
+++ basic/object.js
@@ -30,4 +30,64 @@ userDetails.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 };
 
-console.log(userDetails.
+// Define a new tinderUser object
+const tinderUser = {};
+
+tinderUser.id = "001xyz";
+tinderUser.name = "Arjun";
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "example@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Akash",
+            lastname: "Kumar"
+        }
+    }
+};
+
+console.log(regularUser.fullname.userfullname.firstname);
+
+const object1 = {1: "x", 2: "y"};
+const object2 = {3: "z", 4: "w"};
+const object4 = {5: "a", 6: "b"};
+
+// Combine objects using spread operator
+const combinedObject = {...object1, ...object2, ...object4};
+console.log(combinedObject);
+
+const userList = [
+    {
+        id: 1,
+        email: "user1@gmail.com"
+    },
+    {
+        id: 2,
+        email: "user2@gmail.com"
+    },
+    {
+        id: 3,
+        email: "user3@gmail.com"
+    },
+];
+
+console.log(userList[1].email);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const courseDetails = {
+    courseName: "JavaScript Essentials",
+    price: "499",
+    instructor: "Arjun"
+};
+
+const {instructor} = courseDetails;
+console.log(instructor);
+
