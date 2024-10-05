c29470fd2d268c6a0f406f3971b6c20fe9010cec
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-05 13:42:48 +0530

    array_operations_demo.js

--- basic/basic/array_operations_demo.js
+++ basic/basic/array_operations_demo.js
@@ -1,1 +1,21 @@
+const marvel = [1, 2, 3, 4, 5];
+const dc = [4, 5, 6, 7];
 
+marvel.push(dc);
+console.log(marvel);
+
+const arr1 = [...marvel, ...dc];
+console.log(arr1);
+
+const another = [1, 2, 3, 4, [4, 5, [5, 5, [9, 9]]]];
+const real = another.flat(Infinity);
+console.log(real);
+
+const string = "Akash";
+console.log(Array.isArray(string));
+console.log(Array.from(string));
+
+let a1 = 122;
+let a2 = 222;
+let a3 = 123;
+console.log(Array.of(a1, a2, a3));
