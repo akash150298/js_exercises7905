0988901fd37dba882922c50a36802a3cad3dcfb0
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-05 15:50:51 +0530

    Update iter.js

--- basic/iter.js
+++ basic/iter.js
@@ -1,1 +1,15 @@
+const numbersArray = [1, 3, 5, 6, "JavaScript"];
+for (const element of numbersArray) {
+    console.log(element);
+}
+// Same applies for strings
 
+const countryCodesMap = new Map();
+countryCodesMap.set("Akash", 1);
+countryCodesMap.set(2, "United States of America");
+countryCodesMap.set('FR', "France");
+countryCodesMap.set('IN', "India");
+
+for (const [code, country] of countryCodesMap) {
+    console.log(`Key: ${code} Value: ${country}`);
+}
