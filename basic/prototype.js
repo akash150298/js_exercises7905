8ca72eb73790129223079cce42230304468f79eb
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-08 11:03:55 +0530

    updating js

--- basic/prototype.js
+++ basic/prototype.js
@@ -1,1 +1,12 @@
+function user(name, score) {
+    this.name = name
+    this.score = score
+}
 
+user.prototype.incre = function() {
+    this.score++
+    console.log(this.score)
+}
+
+const user1 = new user("akash", 1)
+console.log(user1.incre())
