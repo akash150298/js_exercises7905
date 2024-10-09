0c5d659bfb7d96abcfc6d2d04721e9f1bbd3c667
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-09 14:06:20 +0530

    practice.js

--- basic/logic.js
+++ basic/logic.js
@@ -64,3 +64,18 @@ setInterval(function ()
   textNode.data = text;
 }, 100);
 }
+
+// logic for leap ear (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+
+//  find whether day 1 january is sunday or not
+for (var year = 2014; year <= 2050; year++) {
+    // Create a Date object for January 1st of the current year
+    var d = new Date(year, 0, 1);
+
+    // Check if January 1st is a Sunday (where Sunday corresponds to day index 0)
+    if (d.getDay() === 0) {
+        // Log a message if January 1st is a Sunday for the current year
+        console.log("1st January is being a Sunday  " + year);
+    }
+}
+
