b5c45dc7f3a3775a2ae1c70d2bd3b2e5f40e61bb
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-09 14:01:36 +0530

    Update logic.js

--- basic/logic.js
+++ basic/logic.js
@@ -50,3 +50,17 @@ if (hour === 0 && prepand === ' AM ') {
 
 // Display the current time
 console.log("Current Time: " + hour + prepand + " : " + minute + " : " + second);
+
+// rotate string
+function animate_string(id) 
+{
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+setInterval(function () 
+{
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}
