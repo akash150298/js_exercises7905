24a4d04ca6c63cc2ea9e0353e5e700d2af15465d
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-16 15:58:34 +0530

    Update callback_and_promise.js

--- basic/callback_and_promise.js
+++ basic/callback_and_promise.js
@@ -132,3 +132,21 @@ promisified(2,3).then((result) => {
 .catch((error)=>{
     console.log(error.message)
 })
+
+function http(url){
+    return new Promise((resolve, reject) {
+        fetch(url)
+        .then((response)=>{
+            if (!response.ok) {
+                throw new Error('HTTP error')
+            }
+            return response.json()
+        })
+        .((data) => {
+            resolve(data)
+        })
+        .catch((error) => {
+            reject(error)
+        })
+    })
+}
