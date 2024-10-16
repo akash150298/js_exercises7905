ee6e0975f1dcfd414b85ce2412f6013c2dcaa266
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-16 10:47:25 +0530

    Update promisechain.js

--- basic/promisechain.js
+++ basic/promisechain.js
@@ -37,3 +37,44 @@ fetchUser()
     .catch(error => {
         console.error('Error:', error);
     });
+//  callback hell
+function fetchUser(callback) {
+    setTimeout(() => {
+        callback(null, { id: 1, name: 'John Doe' });
+    }, 1000);
+}
+
+function fetchUserPosts(userId, callback) {
+    setTimeout(() => {
+        callback(null, ['Post 1', 'Post 2']);
+    }, 1000);
+}
+
+function fetchPostComments(post, callback) {
+    setTimeout(() => {
+        callback(null, [`Comment 1 for ${post}`, `Comment 2 for ${post}`]);
+    }, 1000);
+}
+
+fetchUser((err, user) => {
+    if (err) {
+        return console.error('Error fetching user:', err);
+    }
+    console.log('User:', user);
+
+    fetchUserPosts(user.id, (err, posts) => {
+        if (err) {
+            return console.error('Error fetching posts:', err);
+        }
+        console.log('Posts:', posts);
+
+        fetchPostComments(posts[0], (err, comments) => {
+            if (err) {
+                return console.error('Error fetching comments:', err);
+            }
+            console.log('Comments:', comments);
+        });
+    });
+});
+
+console.log('Fetching user...');
