4ae30749560cf0a31c1a0e596e019852a00393eb
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-17 14:37:06 +0530

    Update parallelPromises.js

--- backend_exercise/parallelPromises.js
+++ backend_exercise/parallelPromises.js
@@ -1,1 +1,32 @@
+function downloadContents(urls) {
+    const promises = urls.map((url) => {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+            .then(response => {
+                if(!resonse.ok){
+                    throw new Error(`Response status ${response.status}`)
+                }
+                return response.text()
+           })
+           .then(data => resolve(data))
+           .catch(error => reject(error))
+        })
+    })
+    return Promise.all(promises)
+}
+
+// Usage example:
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/2',
+  'https://jsonplaceholder.typicode.com/posts/3'
+];
+
+downloadContents(urls)
+  .then(contents => {
+    console.log('Downloaded contents:', contents);
+  })
+  .catch(error => {
+    console.log('Error:', error.message);
+  });
 
