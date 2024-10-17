4a8a450fce8c1d6092cff9c70b6d29caffa19495
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-17 12:31:26 +0530

    Update main.js

--- multhreading/main.js
+++ multhreading/main.js
@@ -1,1 +1,12 @@
+const { fork } = require('child_process');
+const child = fork('child.js');
 
+// Send a message to the child process
+child.send({ hello: 'world' });
+
+// Listen for messages from the child process
+child.on('message', (message) => {
+  console.log('Message from child:', message);
+});
+
+//  child.js is a different file that will be running
