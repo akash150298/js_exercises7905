1891ffcb8e436afb87f1198600203ac6cdde2013
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-17 12:43:19 +0530

    Update webservice.js
    This will show how you can handle traffic through fork process

--- multhreading/webservice.js
+++ multhreading/webservice.js
@@ -1,1 +1,17 @@
 
+const { fork } = require('child_process');
+const http = require('http');
+
+const numCPUs = require('os').cpus().length;
+
+for (let i = 0; i < numCPUs; i++) {
+  fork('worker.js');
+}
+
+// worker.js
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello, world!');
+}).listen(8000);
