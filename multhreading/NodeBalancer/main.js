f2186c16b1f211e295b247b8abca0158e73869d4
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-10-17 13:13:43 +0530

    Creating main.js for multiple sub process

--- multhreading/NodeBalancer/main.js
+++ multhreading/NodeBalancer/main.js
@@ -1,1 +1,7 @@
+const { fork } = require('child_process');
+const numCPUs = require('os').cpus().length;
+
+for (let i = 0; i < numCPUs; i++) {
+  fork('worker.js', [8000 + i]); // Assign different ports to each child process
+}
 
